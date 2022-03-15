0458ff18b2e48f6d940d595214de3dba2a4a6a47
Joseph Tharpe <solowun6@aol.com>
2022-03-14 20:10:36 -0700

    constructors

--- src/com/codewithjoey/Employee.java
+++ src/com/codewithjoey/Employee.java
@@ -4,12 +4,19 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    //CONSTRUCTORS
+    public Employee(int baseSalary, int hourlyRate){
+        //using setters instead of this.x
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
     //METHODS
     public int calculateWage(int extraHours){
         return getBaseSalary() + (extraHours * getHourlyRate());
     }
 
-    public void setBaseSalary(int baseSalary){ //SETTER
+    private void setBaseSalary(int baseSalary){ //SETTER
         if(baseSalary <= 0)
             throw new IllegalArgumentException("Salary cannot be 0 or less");
         this.baseSalary = baseSalary;
@@ -24,7 +31,7 @@ public class Employee {
         return hourlyRate;
     }
 
-    public void setHourlyRate(int hourlyRate) {
+    private void setHourlyRate(int hourlyRate) {
         if(hourlyRate <= 0)
             throw new IllegalArgumentException("Hourly rate cannot be 0 or less");
         this.hourlyRate = hourlyRate;
