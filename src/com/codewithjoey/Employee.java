fab62738c5530084514f25f33a5bf8d998cad781
Joseph Tharpe <solowun6@aol.com>
2022-03-14 19:36:39 -0700

    getters and setters

--- src/com/codewithjoey/Employee.java
+++ src/com/codewithjoey/Employee.java
@@ -1,12 +1,33 @@
 package com.codewithjoey;
 
 public class Employee {
-    //FIELDS
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     //METHODS
     public int calculateWage(int extraHours){
-        return baseSalary + (extraHours * hourlyRate);
+        return getBaseSalary() + (extraHours * getHourlyRate());
     }
+
+    public void setBaseSalary(int baseSalary){ //SETTER
+        if(baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less");
+        this.baseSalary = baseSalary;
+    }
+    //GETTER
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if(hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or less");
+        this.hourlyRate = hourlyRate;
+    }
+
 }
