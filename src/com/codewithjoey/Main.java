e407e2fd6706ff70bec9d8373ef82ca15c9baecc
Joseph Tharpe <solowun6@aol.com>
2022-03-14 20:00:10 -0700

    reducing coupling

--- src/com/codewithjoey/Main.java
+++ src/com/codewithjoey/Main.java
@@ -4,11 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        var employee = new Employee();
-        employee.setBaseSalary(65_000);
-        employee.setHourlyRate(20);
-        int wage = employee.calculateWage(10);
-        System.out.println(wage);
+        var browser = new Browser();
+
+
+//        var employee = new Employee();
+//        employee.setBaseSalary(65_000);
+//        employee.setHourlyRate(20);
+//        int wage = employee.calculateWage(10);
+//        System.out.println(wage);
     }
 
 
